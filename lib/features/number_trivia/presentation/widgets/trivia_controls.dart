c380b5ba9c9f64453916773ae283f13ca2b28112
Arlo78 <yellowstar78@gmail.com>
2022-06-03 12:49:05 +0200

    refactor: move TrivaControls in own file

---
+++ lib/features/number_trivia/presentation/widgets/trivia_controls.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:numbers_trivia_app/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:numbers_trivia_app/features/number_trivia/presentation/bloc/number_trivia_event.dart';
+
+class TriviaControls extends StatefulWidget {
+  const TriviaControls({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<TriviaControls> createState() => _TriviaControlsState();
+}
+
+class _TriviaControlsState extends State<TriviaControls> {
+  final controller = TextEditingController();
+  late String inputString;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          controller: controller,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Input a number',
+          ),
+          onChanged: (value) {
+            inputString = value;
+          },
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                onPressed: dispatchConcrete,
+                child: const Text('Search'),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: ElevatedButton(
+                onPressed: dispatchRandom,
+                child: const Text('Get random trivia'),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  void dispatchConcrete() {
+    controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context).add(GetTriviaForConcreteNumberEvent(inputString));
+  }
+
+  void dispatchRandom() {
+    BlocProvider.of<NumberTriviaBloc>(context).add(GetTriviaForRandomNumberEvent());
+  }
+}
